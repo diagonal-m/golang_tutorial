8b6f75948337e385a2d6d08fdf65e694eb17936b
佐藤太一 <3101taichi@gmail.com>
2023-06-12 18:50:56 +0900

    Return greetings for multiple people

--- boiler-plate/cmd/hello/hello.go
+++ boiler-plate/cmd/hello/hello.go
@@ -2,27 +2,27 @@ package main
 
 import (
     "fmt"
-		"log"
+    "log"
 
     "github.com/satoichi/greetings"
 )
 
 func main() {
-		// Set properties of the predefined Logger, including
+    // Set properties of the predefined Logger, including
     // the log entry prefix and a flag to disable printing
     // the time, source file, and line number.
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    // Get a greeting message and print it.
-    message, err := greetings.Hello("Gladys")
-    // If an error was returned, print it to the console and
-    // exit the program.
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
     if err != nil {
         log.Fatal(err)
     }
-
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
